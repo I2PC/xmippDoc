8f220837d0c12aec0e3bcd0a11e77e136b86d52e
Yunior C. Fonseca Reyna <fonsecareyna82@gmail.com>
2020-07-09 02:39:07 +0200

    removing old doc

--- html/search/files_11.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['qpsolver_2ecpp',['QPSolver.cpp',['../QPSolver_8cpp.html',1,'']]],
-  ['quickhelpjdialog_2ejava',['QuickHelpJDialog.java',['../QuickHelpJDialog_8java.html',1,'']]],
-  ['quickhelppane_2ejava',['QuickHelpPane.java',['../QuickHelpPane_8java.html',1,'']]]
-];
