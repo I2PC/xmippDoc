91697832add35b1b25cddeab17f2d1ad556e17b7
Yunior C. Fonseca Reyna <fonsecareyna82@gmail.com>
2020-07-09 02:06:00 +0200

    Remove old documentation

--- html/search/files_19.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['zvi_5freader_2ejava',['ZVI_Reader.java',['../ZVI__Reader_8java.html',1,'']]]
-];
--- html/search/files_a.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['jmetadataio_2ejava',['JMetaDataIO.java',['../JMetaDataIO_8java.html',1,'']]]
-];
