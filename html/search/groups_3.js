6e1a190a0a360d85a97af15ac26a0ff4bb1c5f3c
biocompwebs <biocompwebs@gmail.com>
2020-07-10 20:16:01 +0200

    buildbot automated-update

---
+++ html/search/groups_3.js
@@ -0,0 +1,14 @@
+var searchData=
+[
+  ['data_20sets',['Data sets',['../group__DataSets.html',1,'']]],
+  ['datatypes_20for_20multidimarrays',['Datatypes for MultidimArrays',['../group__Datatypes.html',1,'']]],
+  ['detect_20missing_20wedge',['Detect missing wedge',['../group__DetectMissingWedge.html',1,'']]],
+  ['diffusion_20maps',['Diffusion Maps',['../group__DiffusionMaps.html',1,'']]],
+  ['dm3_20file_20format',['DM3 File format',['../group__DM3.html',1,'']]],
+  ['dm4_20file_20format',['DM4 File format',['../group__DM4.html',1,'']]],
+  ['document_20files_20_28docfiles_29',['Document Files (Docfiles)',['../group__DocFiles.html',1,'']]],
+  ['determine_20the_20angular_20determination_20accuracy_20of_20a_20set_20of_20particles_20and_20a_203d_20reconstruction_20_28mpi_29',['Determine the angular determination accuracy of a set of particles and a 3D reconstruction (MPI)',['../group__MpiProgAngularAccuracyPCA.html',1,'']]],
+  ['different_20statistics_20of_20the_20pdb',['Different statistics of the PDB',['../group__PDBAnalysis.html',1,'']]],
+  ['deform_20pdb_20according_20to_20nma',['Deform PDB according to NMA',['../group__PDBNMADeform.html',1,'']]],
+  ['deform_20a_20volume_20using_20spherical_20harmonics',['Deform a volume using spherical harmonics',['../group__VolDeformSph.html',1,'']]]
+];
---
+++ html/search/groups_a.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['linear_20algebra',['Linear algebra',['../group__BilibLinearAlgebra.html',1,'']]],
+  ['laplacian_20eigenmap',['Laplacian Eigenmap',['../group__LaplacianEigenmap.html',1,'']]],
+  ['linear_20local_20tangent_20space_20alignment',['Linear Local Tangent Space Alignment',['../group__LLTSA.html',1,'']]],
+  ['linearity_20preserving_20projection',['Linearity Preserving Projection',['../group__LPP.html',1,'']]],
+  ['local_20tangent_20space_20alignment',['Local Tangent Space Alignment',['../group__LTSA.html',1,'']]],
+  ['local_20filter',['Local Filter',['../group__Resolution.html',1,'']]]
+];
