a696ef327e70d135c3c7a3c5c4ecf3d83611d510
biocompwebs <biocompwebs@gmail.com>
2020-07-18 07:02:32 +0200

    buildbot automated-update

---
+++ html/search/groups_2.js
@@ -0,0 +1,23 @@
+var searchData=
+[
+  ['ctf_5festimate_5ffrom_5fmicrograph_20_28ctf_20estimation_20from_20a_20micrograph_29',['ctf_estimate_from_micrograph (CTF estimation from a micrograph)',['../group__AssignCTF.html',1,'']]],
+  ['classification_20first_20split',['Classification first split',['../group__ClassificationFirstSplit.html',1,'']]],
+  ['core_20analysis_20for_20cl2d',['Core analysis for CL2D',['../group__ClassifyCL2DCore.html',1,'']]],
+  ['classification_20with_20fourier_20transform_20based_20translational_20and_20rotational_20invariants',['Classification with Fourier Transform based Translational and Rotational Invariants',['../group__ClassifyFTTRI.html',1,'']]],
+  ['classify_20a_20set_20of_20images_20into_20a_20discrete_20set_20of_20classes',['Classify a set of images into a discrete set of classes',['../group__ClassifySignificant.html',1,'']]],
+  ['code_20book',['Code book',['../group__CodeBook.html',1,'']]],
+  ['common_20lines_20_28find_20common_20lines_20between_20projections_29',['Common Lines (find common lines between projections)',['../group__CommonLinesProgram.html',1,'']]],
+  ['compare_20classes',['Compare Classes',['../group__CompareClasses.html',1,'']]],
+  ['convertvol2pseudo',['ConvertVol2Pseudo',['../group__ConvertVol2Pseudo.html',1,'']]],
+  ['coordinates_20micrograph_20filter',['Coordinates Micrograph Filter',['../group__CoordinatesMicrographFilters.html',1,'']]],
+  ['ctf_20phase_20flip_20in_202d_20particles',['CTF Phase flip in 2D particles',['../group__Correct.html',1,'']]],
+  ['ctf_5fcorrect_5famplitude3d_20_283d_20wiener_20filtering_29',['ctf_correct_amplitude3D (3D Wiener filtering)',['../group__CorrectAmplitude3D.html',1,'']]],
+  ['ctf_5fgroup',['ctf_group',['../group__CTFGroup.html',1,'']]],
+  ['ctf_20support_20classes',['CTF support classes',['../group__CTFSupport.html',1,'']]],
+  ['ctf_5fcorrect_5fidr_20_28iterative_20data_20refinement_20for_20ctf_20amplitude_20correction_29',['ctf_correct_idr (Iterative Data Refinement for CTF amplitude correction)',['../group__IDR.html',1,'']]],
+  ['classifier',['Classifier',['../group__KNN.html',1,'']]],
+  ['component_20analysis',['Component Analysis',['../group__Neighbourhood.html',1,'']]],
+  ['construct_20a_20low_20and_20high_20resolution_20dictionary',['Construct a low and high resolution dictionary',['../group__PDBConstructDictionary.html',1,'']]],
+  ['convert_5fpdb2vol_20_28pdb_20phantom_20program_29',['convert_pdb2vol (PDB Phantom program)',['../group__PDBPhantom.html',1,'']]],
+  ['change_20the_20image_20gray_20values_20to_20match_20a_20volume_20projection',['Change the image gray values to match a volume projection',['../group__TransformImageGray.html',1,'']]]
+];
