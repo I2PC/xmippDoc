81bd69b6d5ad0523ce6aa3c7d86e8c1f75df3467
biocompwebs <biocompwebs@gmail.com>
2020-08-04 21:00:13 +0200

    buildbot automated-update

---
+++ html/search/namespaces_1.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['core',['core',['../namespacecore.html',1,'']]],
+  ['ctpl',['ctpl',['../namespacectpl.html',1,'']]],
+  ['cudaversion',['CudaVersion',['../namespacecuFFTAdvisor_1_1CudaVersion.html',1,'cuFFTAdvisor']]],
+  ['cufftadvisor',['cuFFTAdvisor',['../namespacecuFFTAdvisor.html',1,'']]],
+  ['detail',['detail',['../namespacectpl_1_1detail.html',1,'ctpl']]],
+  ['tristate',['Tristate',['../namespacecuFFTAdvisor_1_1Tristate.html',1,'cuFFTAdvisor']]]
+];
