994e8723f10b45e9403d35e0cb2103ef392da738
biocompwebs <biocompwebs@gmail.com>
2020-08-29 07:35:49 +0200

    buildbot automated-update

---
+++ html/search/defines_8.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['half_5fcircles',['HALF_CIRCLES',['../polar_8h.html#af26ff3810bc37be1f0c6e3a3cd86e3c7',1,'polar.h']]],
+  ['highpass',['HIGHPASS',['../fourier__filter_8h.html#ab55942c743e16f6629ad144214ced198',1,'fourier_filter.h']]]
+];
---
+++ html/search/enumvalues_12.js
@@ -0,0 +1,17 @@
+var searchData=
+[
+  ['shears',['SHEARS',['../group__ProjectionProgram.html#gga02c32f1ab8b5e69f891e150cae9b9f64a3879f1968a9c989ab6b1859549fc62db',1,'project.h']]],
+  ['sigmoid',['SIGMOID',['../svm_8h.html#a99fb83031ce9923c84392b4e92f956b5a11c1096689b7d3504dbcc4f61d854883',1,'svm.h']]],
+  ['simply_5fclosed_5ffringes',['SIMPLY_CLOSED_FRINGES',['../group__FringeProcessing.html#ggab0a9a5fb1f868f9e5e4fc1b76ce280cfa61b80aab02c4bcc13afb1ba4edc9f4ec',1,'fringe_processing.h']]],
+  ['simply_5fclosed_5ffringes_5fmod',['SIMPLY_CLOSED_FRINGES_MOD',['../group__FringeProcessing.html#ggab0a9a5fb1f868f9e5e4fc1b76ce280cfa2e8543d94cd3b15ecf47de3f81ee15c1',1,'fringe_processing.h']]],
+  ['simply_5fopen_5ffringes',['SIMPLY_OPEN_FRINGES',['../group__FringeProcessing.html#ggab0a9a5fb1f868f9e5e4fc1b76ce280cfa65c0ba796163ee6c57b5eba307097a75',1,'fringe_processing.h']]],
+  ['sirt',['SIRT',['../classBasicARTParameters.html#a69792cca4b1fd1a00975a7c9c1bcdf02ab6bfee5169d8c3bde955bef8ce0d60bb',1,'BasicARTParameters']]],
+  ['sizemode',['SIZEMODE',['../classProgWindow.html#aa10431cf5ac8f466bef1da0f2a4c0b02aafca8530e9f34408d3e80fef85a9970a',1,'ProgWindow']]],
+  ['smooth',['SMOOTH',['../classProgTransformDownsample.html#a0b06796a2d03b08be229d6a74855e7ada8eef4567933e785163d826d609e978e8',1,'ProgTransformDownsample']]],
+  ['soft_5fthresholding',['SOFT_THRESHOLDING',['../classWaveletFilter.html#a47778df5ceeab1affae28fa5b79f9147a25e1d25e4f823321300e1ba2c156f408',1,'WaveletFilter']]],
+  ['splines',['splines',['../classBasis.html#aa73185836d4dd144f8591d827d8f952ca22d68f5f02d77c712461e2dd630873b9',1,'Basis']]],
+  ['splitsample',['SPLITSAMPLE',['../classClassificationTrainingSet.html#a6a9113b8f98ea09c5d3bf5917c8315afaccc860141b1001cbabd43eee06058fc3',1,'ClassificationTrainingSet']]],
+  ['standard',['Standard',['../group__Images.html#ggaa65f3b21a9a92ff022e435a7304126d2a57f9bf199e30a749a03be1dac2f5f624',1,'xmipp_image_base.h']]],
+  ['substraction',['SUBSTRACTION',['../group__MetaData.html#gga29d933d0f4ff963ca56745a8da93526bad752627e1f4f8d9cf11c8b6e2c9da625',1,'metadata_sql.h']]],
+  ['swiss',['SWISS',['../group__DimRedTools.html#gga222ba6818229095ef9c87df89fcdc779a0d84e07e1fcf14b233e7a1f4a25e6d9e',1,'dimred_tools.h']]]
+];
