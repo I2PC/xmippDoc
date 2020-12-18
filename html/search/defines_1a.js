ce0235414ee33d77897f55054bbe6c25f6c28a71
biocompwebs <biocompwebs@gmail.com>
2020-12-18 12:00:10 +0100

    buildbot automated-update

---
+++ html/search/defines_1a.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['z0',['z0',['../blobs_8cpp.html#a3f5c4dfb0dff56801752771f78265fbe',1,'z0():&#160;blobs.cpp'],['../splines_8cpp.html#a3f5c4dfb0dff56801752771f78265fbe',1,'z0():&#160;splines.cpp']]],
+  ['zeps',['ZEPS',['../numerical__recipes_8cpp.html#a1480be118a74bd98a735eecc3ee0e833',1,'numerical_recipes.cpp']]],
+  ['zernike_5forder',['ZERNIKE_ORDER',['../xmipp__polynomials_8cpp.html#a0e8d94fb65b206776f0d626fbc041f58',1,'xmipp_polynomials.cpp']]],
+  ['zf',['zF',['../blobs_8cpp.html#a14105579fd3a215f98f182c2347fb774',1,'zF():&#160;blobs.cpp'],['../splines_8cpp.html#a14105579fd3a215f98f182c2347fb774',1,'zF():&#160;splines.cpp']]]
+];
---
+++ html/search/groups_c.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['new_5fprojmatch_20_28discrete_20angular_20assignment_20using_20a_20new_20projection_20matching_29',['new_projmatch (Discrete angular assignment using a new projection matching)',['../group__angular__projection__matching.html',1,'']]],
+  ['naive_20bayes_20classifier',['Naive Bayes Classifier',['../group__NaiveBayesClassifier.html',1,'']]],
+  ['normalization_20of_20images_20and_20volumes',['Normalization of images and volumes',['../group__Normalize.html',1,'']]],
+  ['numerical_20integration',['Numerical integration',['../group__NumericalIntegration.html',1,'']]],
+  ['numerical_20tools',['Numerical Tools',['../group__NumericalTools.html',1,'']]]
+];
