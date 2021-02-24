251f0e243c47c3fae5e88cf86771ee597cd34d31
Yunior C Fonseca <cfonseca@cnb.csic.es>
2021-02-24 14:37:25 +0100

    buildbot automated-update

---
+++ html/search/groups_b.js
@@ -0,0 +1,28 @@
+var searchData=
+[
+  ['moving_20average',['Moving average',['../group__BilibMA.html',1,'']]],
+  ['morphology',['Morphology',['../group__BilibMorphology.html',1,'']]],
+  ['masks_20in_20fourier_20space',['Masks in Fourier space',['../group__FourierMasks.html',1,'']]],
+  ['mathematical_20operations_20of_20images_20and_20volumes',['Mathematical operations of images and volumes',['../group__ImageOperate.html',1,'']]],
+  ['macros',['Macros',['../group__Macros.html',1,'']]],
+  ['make_20spectra',['Make spectra',['../group__MakeSpectra.html',1,'']]],
+  ['masks',['Masks',['../group__Masks.html',1,'']]],
+  ['mathematical_20morphology',['Mathematical morphology',['../group__MathematicalMorphology.html',1,'']]],
+  ['matrix2d_20matrices',['Matrix2D Matrices',['../group__Matrices.html',1,'']]],
+  ['memory_20management_20for_20numerical_20recipes',['Memory management for numerical recipes',['../group__MemoryManaging.html',1,'']]],
+  ['metadata_20stuff',['Metadata Stuff',['../group__MetaData.html',1,'']]],
+  ['micrograph_20downsample',['Micrograph Downsample',['../group__MicrographDownsample.html',1,'']]],
+  ['micrographs',['Micrographs',['../group__Micrographs.html',1,'']]],
+  ['maximum_20likelihood_20for_20tomograms',['Maximum likelihood for tomograms',['../group__ml__tomo.html',1,'']]],
+  ['ml_5falign2d_20_28maximum_20likelihood_20in_202d_29',['ml_align2d (Maximum likelihood in 2D)',['../group__MLalign2D.html',1,'']]],
+  ['mlf_5falign2d_20_28maximum_20likelihood_20in_202d_20in_20fourier_20space_29',['mlf_align2d (Maximum likelihood in 2D in Fourier space)',['../group__MLFalign2D.html',1,'']]],
+  ['movie_20alignment_20by_20correlation',['Movie alignment by correlation',['../group__MovieAlignmentCorrelation.html',1,'']]],
+  ['mpi',['MPI',['../group__MPI.html',1,'']]],
+  ['mpi_5fml',['MPI_ML',['../group__MPI__ML.html',1,'']]],
+  ['mpiprogangularprojectionmatching',['MpiProgAngularProjectionMatching',['../group__MpiProgAngularProjectionMatching.html',1,'']]],
+  ['mrc_20file_20format',['MRC File format',['../group__MRC.html',1,'']]],
+  ['multidimensional_20arrays',['Multidimensional Arrays',['../group__MultidimensionalArrays.html',1,'']]],
+  ['ml_5frefine3d_20_28maximum_20likelihood_203d_20refinement_29',['ml_refine3d (Maximum likelihood 3D refinement)',['../group__Refine3d.html',1,'']]],
+  ['measure_20resolution_20through_20the_20inverse_20of_20border_20widths',['Measure resolution through the inverse of border widths',['../group__ResolutionIBW.html',1,'']]],
+  ['matrix1d_20vectors',['Matrix1D Vectors',['../group__Vectors.html',1,'']]]
+];
