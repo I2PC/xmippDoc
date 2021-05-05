76182d9cbdbe8222dca2e794cf526b969945f7b7
biocompwebs <biocompwebs@gmail.com>
2021-05-05 23:55:02 +0200

    buildbot automated-update

---
+++ html/search/defines_11.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['q0',['q0',['../common__lines_8cpp.html#ac61556e19676a2bd8cefe184427ad84e',1,'common_lines.cpp']]],
+  ['q1',['q1',['../common__lines_8cpp.html#a2727b8084704029433d23fb39cd41e0a',1,'common_lines.cpp']]],
+  ['q2',['q2',['../common__lines_8cpp.html#af3dba398bf5802642ccbd218a3f8284b',1,'common_lines.cpp']]],
+  ['q3',['q3',['../common__lines_8cpp.html#adf7cf797e84ae11a90fdfabb3bb388fd',1,'common_lines.cpp']]]
+];
---
+++ html/search/groups_12.js
@@ -0,0 +1,12 @@
+var searchData=
+[
+  ['validate_20a_20volume_20analyzing_20the_20clusterability_20of_20each_20projection_20image_20_28mpi_29',['validate a volume analyzing the clusterability of each projection image (MPI)',['../group__MpiProgValidationNonTilt.html',1,'']]],
+  ['vector_20volumes',['Vector volumes',['../group__Vectorial.html',1,'']]],
+  ['vector_20operations',['Vector Operations',['../group__VectorOperations.html',1,'']]],
+  ['virus',['Virus',['../group__VirusInterface.html',1,'']]],
+  ['volume_20pca',['Volume PCA',['../group__VolumePCA.html',1,'']]],
+  ['volume_20segmentation',['Volume segmentation',['../group__VolumeSegment.html',1,'']]],
+  ['validation_20of_20volume_20consistency_20with_20respect_20to_20the_20provided_20classes',['Validation of volume consistency with respect to the provided classes',['../group__VolumeValidatePCA.html',1,'']]],
+  ['vector_20quantization_20for_20projections',['Vector Quantization for Projections',['../group__VQforProjections.html',1,'']]],
+  ['vector_20quantization_20for_20volumes',['Vector Quantization for Volumes',['../group__VQforVolumes.html',1,'']]]
+];
