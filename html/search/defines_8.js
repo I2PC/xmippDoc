919a7c1709b9838eab8c16480245b5e7a97b3884
biocompwebs <biocompwebs@gmail.com>
2021-06-07 20:22:10 +0200

    buildbot automated-update

--- html/search/defines_8.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['half_5fcircles',['HALF_CIRCLES',['../polar_8h.html#af26ff3810bc37be1f0c6e3a3cd86e3c7',1,'polar.h']]],
-  ['hex',['HEX',['../CMakeCXXCompilerId_8cpp.html#a46d5d95daa1bef867bd0179594310ed5',1,'CMakeCXXCompilerId.cpp']]],
-  ['highpass',['HIGHPASS',['../fourier__filter_8h.html#ab55942c743e16f6629ad144214ced198',1,'fourier_filter.h']]]
-];
