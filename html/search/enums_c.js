80deecf1e39922c05be0c554756981ba2eb91125
biocompwebs <biocompwebs@gmail.com>
2022-04-14 05:24:49 +0200

    buildbot automated-update

---
+++ html/search/enums_c.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['neightype',['neighType',['../classSOM.html#a744005a47023f93c78799e9323e6cb6d',1,'SOM']]],
+  ['normalizationmode',['NormalizationMode',['../classProgNormalize.html#a5943c041e4e52e1cefaa716e1957973b',1,'ProgNormalize']]]
+];
